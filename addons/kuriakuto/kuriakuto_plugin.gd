3fae886d98850b35116b9226fa1650e666edc4e9
PolanskiPol <polanski.real@gmail.com>
2024-08-09 19:19:00 +0200

    feat: Add KuriakutoCore to autoload from plugin

--- addons/kuriakuto/kuriakuto_plugin.gd
+++ addons/kuriakuto/kuriakuto_plugin.gd
@@ -2,3 +2,9 @@
 extends EditorPlugin
 
 ## This is an empty script so Godot detects Kuriakuto as a Plugin and adds the Kuriakuto custom nodes to the "Add node" window.
+
+func _enter_tree() -> void:
+	add_autoload_singleton("KuriakutoCore", "kuriakuto_core.gd")
+	
+func _exit_tree() -> void:
+	remove_autoload_singleton("KuriakutoCore")
